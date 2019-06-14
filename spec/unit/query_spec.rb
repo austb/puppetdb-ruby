a05c6d3b3de4ce3a5ebb2e0d1d0568c814bab82d
Austin Blatt <austinblatt@gmail.com>
2019-06-14 14:13:38 -0700

    (maint) Fix tests that could catch other errors

--- spec/unit/query_spec.rb
+++ spec/unit/query_spec.rb
@@ -112,7 +112,7 @@ describe 'SSL support' do
         cert: 'bar'
       }
 
-      -> { PuppetDB::Client.new(settings) }.should_not raise_error(RuntimeError)
+      -> { PuppetDB::Client.new(settings) }.should_not raise_error
     end
   end
 
